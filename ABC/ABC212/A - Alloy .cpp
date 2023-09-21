52219d5f5cfa7aa4c7d88cd29d7b6ec95743176e
TaichiIwamoto <131168152+TaichiIwamoto@users.noreply.github.com>
2023-09-21 20:53:03 +0900

    A-Alloy solved

---
+++ ABC/ABC212/A - Alloy .cpp
@@ -0,0 +1,77 @@
+#ifndef INCLUDED_MAIN
+#define INCLUDED_MAIN
+
+#include __FILE__
+
+int main()
+{
+	vector<int> vec(2);
+	tmpCin(vec);
+	if (0 < vec[0] && vec[1] == 0) {
+		cout << "Gold" << endl;
+	}
+	else if (vec[0] == 0 && 0 < vec[1]) {
+		cout << "Silver" << endl;
+	}
+	else if (0 < vec[0] && 0 < vec[1]) {
+		cout << "Alloy" << endl;
+	}
+	return 0;
+}
+
+#else // !INCLUDED_MAIN
+
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <cstring>
+#include <algorithm>
+using namespace std;
+template <class T>
+//2‚Â‚Ì—v‘f‚ÌÅ‘å’l
+bool chmax(T& a, const T& b) {
+	if (a < b) {
+		a = b;
+	}
+	return a < b;
+}
+
+//2‚Â‚Ì—v‘f‚ÌÅ¬’l
+template <class T>
+bool chmin(T& a, const T& b) {
+	if (b < a) {
+		a = b;
+	}
+	return b < a;
+}
+
+//vector‚Ì—v‘f•¶•W€“ü—Í‚·‚é
+template <class T>
+void tmpCin(vector<T>& vec) {
+	for (int i = 0; i < (int)vec.size(); i++) {
+		cin >> vec.at(i);
+	}
+}
+
+//string‚ğ‹t‡‚É‚µ‚Ä•Ô‚·
+template <class T>
+string reverseStr(T str) {
+	string strR = str;
+	reverse(strR.begin(), strR.end());
+	return strR;
+}
+
+//‰ñ•ª”»’è
+template <class T>
+bool isPalindrome(T str) {
+	int strSize = str.size();
+	for (int i = 0; i < strSize; i++) {
+		if (str[i] != str[strSize - i - 1]) {
+			return false;
+		}
+	}
+	return true;
+}
+
+
+#endif //INCLUDED_MAINN
