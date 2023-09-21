87e6075ab1f71a72f8936236b6480108ec69ddc8
TaichiIwamoto <131168152+TaichiIwamoto@users.noreply.github.com>
2023-09-21 20:53:11 +0900

    template updated

--- template.cpp
+++ template.cpp
@@ -13,23 +13,31 @@ int main()
 #include <iostream>
 #include <vector>
 #include <cmath>
+#include <cstring>
+#include <algorithm>
 using namespace std;
 template <class T>
 //2つの要素の最大値
 bool chmax(T& a, const T& b) {
-	if (a < b)a = b; return a < b;
+	if (a < b) {
+		a = b;
+	}
+	return a < b;
 }
 
 //2つの要素の最小値
 template <class T>
 bool chmin(T& a, const T& b) {
-	if (b < a)a = b; return b < a;
+	if (b < a) {
+		a = b;
+	}
+	return b < a;
 }
 
 //vectorの要素文標準入力する
 template <class T>
 void tmpCin(vector<T>& vec) {
-	for (int i = 0; i < vec.size(); i++) {
+	for (int i = 0; i < (int)vec.size(); i++) {
 		cin >> vec.at(i);
 	}
 }
@@ -55,4 +63,4 @@ bool isPalindrome(T str) {
 }
 
 
-#endif //INCLUDED_MAIN
+#endif //INCLUDED_MAINN
